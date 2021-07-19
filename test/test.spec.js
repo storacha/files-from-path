ffbb5239ac9bb84a0eb04d52dda1e6f936342e31
Vasco Santos <vasco.santos@moxy.studio>
2021-07-19 11:08:13 +0200

    feat: get files

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,12 +1,18 @@
 import test from 'ava'
 import process from 'process'
-import { filesFromPath } from '../src/index.js'
+import { filesFromPath, getFilesFromPath } from '../src/index.js'
 
-test('joins CARs', async t => {
+test('yields files from fixtures folder', async t => {
   const files = []
   for await (const f of filesFromPath(`${process.cwd()}/test/fixtures`)) {
     files.push(f)
   }
+
+  t.true(files.length === 2)
+})
+
+test('gets files from fixtures folder', async t => {
+  const files = await getFilesFromPath(`${process.cwd()}/test/fixtures`)
 
   t.true(files.length === 2)
 })
