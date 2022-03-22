2c631c4c126ef9caa744cbebf57cc4ef82db9828
Alan Shaw <alan.shaw@protocol.ai>
2022-03-22 17:26:14 +0000

    fix: avoid ulimit maxfiles

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,6 +1,10 @@
 import test from 'ava'
 import Path from 'path'
 import process from 'process'
+import os from 'os'
+import fs from 'fs'
+import crypto from 'crypto'
+import unlimited from 'unlimited'
 import { filesFromPath, getFilesFromPath } from '../src/index.js'
 
 test('yields files from fixtures folder', async t => {
@@ -32,3 +36,48 @@ test('removes custom prefix', async t => {
     t.false(f.name.includes('fixtures'))
   })
 })
+
+test('allows read of more files than ulimit maxfiles', async t => {
+  let dir
+  try {
+    console.log('Generating test data...')
+    dir = await generateTestData()
+    console.log('Done generating test data!')
+    unlimited(256)
+    let totalFiles = 0
+    let totalBytes = 0
+    const readPromises = []
+    for await (const f of filesFromPath(dir)) {
+      readPromises.push((async () => {
+        totalFiles++
+        let i = 0
+        for await (const d of f.stream()) {
+          if (i === 0) { // make slow so we open loads of files
+            await new Promise(resolve => setTimeout(resolve, 1000))
+          }
+          totalBytes += d.length
+          i++
+        }
+      })())
+    }
+    await Promise.all(readPromises)
+    t.is(totalFiles > 0, true)
+    t.is(totalBytes > 0, true)
+  } finally {
+    if (dir) {
+      await fs.promises.rm(dir, { recursive: true, force: true })
+    }
+  }
+})
+
+async function generateTestData () {
+  const dirName = Path.join(os.tmpdir(), `files-from-path-test-${Date.now()}`)
+  await fs.promises.mkdir(dirName)
+  const minBytes = 1024
+  const maxBytes = 1024 * 1024 * 5
+  for (let i = 0; i < 10750; i++) {
+    const numBytes = Math.floor(Math.random() * (maxBytes - minBytes) + minBytes)
+    await fs.promises.writeFile(Path.join(dirName, `${i}.json`), crypto.randomBytes(numBytes))
+  }
+  return dirName
+}
