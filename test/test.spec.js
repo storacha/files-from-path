b4cb447a33394dcacbb9a830617d3d8afaa04e5e
Alan Shaw <alan.shaw@protocol.ai>
2022-03-22 17:34:00 +0000

    fix: no need to generate so many files

--- test/test.spec.js
+++ test/test.spec.js
@@ -40,29 +40,30 @@ test('removes custom prefix', async t => {
 test('allows read of more files than ulimit maxfiles', async t => {
   let dir
   try {
-    console.log('Generating test data...')
-    dir = await generateTestData()
-    console.log('Done generating test data!')
-    unlimited(256)
-    let totalFiles = 0
-    let totalBytes = 0
+    const totalFiles = 512
+    dir = await generateTestData(totalFiles)
+
+    // Restrict to 256 open files max
+    // Note: this doesn't work on MacOS Monterey, you need to run:
+    // sudo sysctl kern.maxfilesperproc=256
+    unlimited(totalFiles / 2)
+
+    let totalReadFiles = 0
     const readPromises = []
     for await (const f of filesFromPath(dir)) {
       readPromises.push((async () => {
-        totalFiles++
+        totalReadFiles++
         let i = 0
-        for await (const d of f.stream()) {
-          if (i === 0) { // make slow so we open loads of files
-            await new Promise(resolve => setTimeout(resolve, 1000))
+        for await (const _ of f.stream()) { // eslint-disable-line no-unused-vars
+          if (i === 0) { // make slow so we open all the files
+            await new Promise(resolve => setTimeout(resolve, 5000))
           }
-          totalBytes += d.length
           i++
         }
       })())
     }
     await Promise.all(readPromises)
-    t.is(totalFiles > 0, true)
-    t.is(totalBytes > 0, true)
+    t.is(totalReadFiles, totalFiles)
   } finally {
     if (dir) {
       await fs.promises.rm(dir, { recursive: true, force: true })
@@ -70,12 +71,12 @@ test('allows read of more files than ulimit maxfiles', async t => {
   }
 })
 
-async function generateTestData () {
+async function generateTestData (n) {
   const dirName = Path.join(os.tmpdir(), `files-from-path-test-${Date.now()}`)
   await fs.promises.mkdir(dirName)
   const minBytes = 1024
   const maxBytes = 1024 * 1024 * 5
-  for (let i = 0; i < 10750; i++) {
+  for (let i = 0; i < n; i++) {
     const numBytes = Math.floor(Math.random() * (maxBytes - minBytes) + minBytes)
     await fs.promises.writeFile(Path.join(dirName, `${i}.json`), crypto.randomBytes(numBytes))
   }
